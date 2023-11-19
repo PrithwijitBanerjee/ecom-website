a2b33e4a346e190967eb7e42764cedf4db2ec3a0
Prithwijit_Banerjee <prithwijit98@gmail.com>
2023-11-19 22:35:15 +0530

    This is my first commit about to store product listing page

---
+++ src/Components/Cores/CoreHome/HeaderSearch.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../../Css/headerSearch.css";
+const HeaderSearch = () => {
+  return (
+    <>
+      <form className="search-bar">
+        <label htmlFor="searchInput" className="search-label">
+          What are you looking for?
+        </label>
+        <div className="search-wrapper">
+          <input
+            type="text"
+            id="searchInput"
+            className="search-input"
+            placeholder="Search Entire Store Here..."
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default HeaderSearch;
---
+++ src/Components/Cores/CoreHome/HomeFooterLogo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HomeFooterLogo = () => {
+  return (
+    <>
+      <Link to={'/productList'}>
+        <div className='footer-style-logo'>
+          <img src='https://prod.aaw.com/media/weltpixel/owlcarouselslider/images/t/a/taf-apparel_01_2.jpg' alt='' className='dimension' />
+          <img src='https://prod.aaw.com/media/weltpixel/owlcarouselslider/images/t/a/taf-apparel_02_2.jpg' alt='' className='dimension' />
+          <img src='https://prod.aaw.com/media/weltpixel/owlcarouselslider/images/t/a/taf-apparel_03_2.jpg' alt='' className='dimension' />
+        </div>
+      </Link>
+    </>
+  )
+}
+
+export default HomeFooterLogo
